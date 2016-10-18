297a4709dacbd6c050a18cac86d393a673bf1ed3
Moreno B <moreno_bralts@hotmail.com>
2016-10-18 13:35:20 +0200

    Updated Camera controls to include Zooming & Rotating

--- SDM-Intersection Simulator/Assets/Scripts/CameraController.cs
+++ SDM-Intersection Simulator/Assets/Scripts/CameraController.cs
@@ -4,29 +4,59 @@
 public class CameraController : MonoBehaviour
 {
 
-    [Range(1, 15)]
-    public int moveSpeed = 1;
+    [Header("Movement Controls")]
+    [Range(15, 50)]
+    public int moveSpeed = 25;
+
+    [Header("Zoom Controls")]
+    public float zoomSpeed = 5.0f;
+
+    [Header("Rotation Contorls")]
+    public float minX = -360.0f;
+    public float maxX = 360.0f;
+
+    public float minY = -45.0f;
+    public float maxY = 45.0f;
+
+    public float sensX = 100.0f;
+    public float sensY = 100.0f;
+
+    float rotationY = 0.0f;
+    float rotationX = 0.0f;
 
     void Update()
     {
 
         Vector3 curPosition = transform.position;
 
-        if (Input.GetKey(KeyCode.W))
+
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector3(curPosition.x, curPosition.y, curPosition.z + moveSpeed);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = new Vector3(curPosition.x, curPosition.y, curPosition.z - moveSpeed);
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector3(curPosition.x - moveSpeed, curPosition.y, curPosition.z);
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.W))
         {
-            transform.position = new Vector3(curPosition.x + moveSpeed, curPosition.y, curPosition.z);
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, minY, maxY);
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
+
     }
 }
