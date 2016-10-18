618880ae440147ef44df8984528b8821d54b702a
Moreno B <moreno_bralts@hotmail.com>
2016-10-18 13:19:26 +0200

    Added simple camera controller

---
+++ SDM-Intersection Simulator/Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+
+    [Range(1, 15)]
+    public int moveSpeed = 1;
+
+    void Update()
+    {
+
+        Vector3 curPosition = transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(curPosition.x, curPosition.y, curPosition.z + moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(curPosition.x, curPosition.y, curPosition.z - moveSpeed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(curPosition.x - moveSpeed, curPosition.y, curPosition.z);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(curPosition.x + moveSpeed, curPosition.y, curPosition.z);
+        }
+    }
+}
