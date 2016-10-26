c831f0347d4183e10b3e2dcb890c178aece026d3
Moreno B <moreno_bralts@hotmail.com>
2016-10-26 10:25:19 +0200

    Added base TrafficLight class to control the light of the trafficlights

---
+++ SDM-Intersection Simulator/Assets/Scripts/Trafficlight.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class Trafficlight : MonoBehaviour {
+
+    public MeshRenderer headModel;
+    public enum eTrafficState { NONE, GREEN, ORANGE, RED };
+
+    private eTrafficState trafficState = eTrafficState.NONE;
+    public eTrafficState TrafficState
+    {
+        get { return TrafficState; }
+        set
+        {
+            if (trafficState == value)
+                return;
+        }
+    }
+
+    private Color GetColorByLightIndex(byte lightIndex)
+    {
+        Color ret = Color.cyan;
+        switch(lightIndex)
+        {
+            case 1:
+                ret = Color.yellow;
+                break;
+
+            case 3:
+                ret = Color.red;
+                break;
+
+            case 4:
+                ret = Color.green;
+                break;
+        }
+
+        return ret;
+    }
+
+    private void UpdateColor()
+    {
+        Color baseColor = Color.cyan;
+        byte lightIndex = 0;
+
+        switch(TrafficState)
+        {
+            case eTrafficState.GREEN:
+                lightIndex = 4;
+                break;
+
+            case eTrafficState.ORANGE:
+                lightIndex = 1;
+                break;
+
+            case eTrafficState.RED:
+                lightIndex = 3;
+                break;
+        }
+
+        for (int i = 0; i < headModel.materials.Length; i++)
+        {
+            float emission = 0;
+
+            if (i == lightIndex)
+                emission = 1;
+
+            baseColor = GetColorByLightIndex(lightIndex);
+
+            Color finalColor = baseColor * Mathf.LinearToGammaSpace(emission);
+
+            headModel.materials[i].SetColor("_EmissionColor", finalColor);
+
+        }
+
+    }
+}
