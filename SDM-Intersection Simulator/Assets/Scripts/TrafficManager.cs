858b8c505ef9be0c48577426056d7fdfd65a9a0f
Moreno B <moreno_bralts@hotmail.com>
2016-10-26 12:50:12 +0200

    Added TrafficManager class which will controll all the trafficlights

---
+++ SDM-Intersection Simulator/Assets/Scripts/TrafficManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrafficManager : Singleton<TrafficManager> {
+
+    public List<Trafficlight> trafficLights = new List<Trafficlight>();
+
+    public void SetTrafficLightState(string id, Trafficlight.eTrafficState newTrafficLightState)
+    {
+        for (int i = 0; i < trafficLights.Count; i++)
+        {
+            Trafficlight trafficLight = trafficLights[i];
+            if (trafficLight == null) continue;
+
+            if (trafficLight.Id != id) continue;
+
+            trafficLight.TrafficState = newTrafficLightState;
+        }
+    }
+}
