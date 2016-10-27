d291492fb0039679bb043ab9325db8dac24ca2af
Moreno B <moreno_bralts@hotmail.com>
2016-10-27 16:33:44 +0200

    Prevented cars to collide with eachother by attaching front and back colliders which will freeze the car movements when in range of an other car.

---
+++ SDM-Intersection Simulator/Assets/Scripts/FreezeInContactWithEntities.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Vehicles.Car;
+
+[RequireComponent(typeof(BoxCollider))]
+public class FreezeInContactWithEntities : MonoBehaviour {
+
+    public CarController carController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("MovingEntity"))
+        {
+            carController.SetFreezeCar(true);
+        }
+    }
+
+    private void OnTriggerLeave(Collider other)
+    {
+        if (other.CompareTag("MovingEntity"))
+        {
+            carController.SetFreezeCar(false);
+        }
+    }
+}
