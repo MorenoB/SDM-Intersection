6fd5f14a79ecf5f5817e9335c55d30a45a336f0d
Moreno B <moreno_bralts@hotmail.com>
2016-11-01 12:49:54 +0100

    Added simple websocket client

---
+++ SDM-Intersection Simulator/Assets/Scripts/Communication/Server-Client/Client.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Client : MonoBehaviour {
+
+    WebSocket w;
+    IEnumerator Start()
+    {
+        w = new WebSocket(new Uri("ws://localhost:8080/Laputa"));
+        yield return StartCoroutine(w.Connect());
+        Send("Hi there");
+        int i = 0;
+        while (true)
+        {
+            string reply = w.RecvString();
+            if (reply != null)
+            {
+                Debug.Log("Received: " + reply);
+            }
+            if (w.error != null)
+            {
+                Debug.LogError("Error: " + w.error);
+                break;
+            }
+            yield return 0;
+        }
+        w.Close();
+    }
+
+    private void Update()
+    { }
+
+    private void Send(string message)
+    {
+        if (w == null)
+            return;
+        w.SendString(message);
+        Debug.Log("Sent " + message);
+    }
+}
+
