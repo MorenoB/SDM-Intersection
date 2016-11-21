7e7e53ac8b1329a17a13c16d074b0c23704f3434
Moreno B <moreno_bralts@hotmail.com>
2016-11-21 16:27:38 +0100

    Re-ordered trafficlanedata in the TrafficManager to make use of a seperate list per vehicle type

---
+++ SDM-Intersection-Simulator/Assets/Editor/MoveVariableData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class MoveVariableData : EditorWindow
+{
+    [MenuItem("Window/MoveVariableData")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(MoveVariableData));
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("MoveData"))
+        {
+            TrafficManager[] dts = Resources.FindObjectsOfTypeAll<TrafficManager>();
+            foreach (TrafficManager dt in dts)
+            {
+                //dt.carLanes = dt.trafficLanes;
+            }
+        }
+    }
+}
--- SDM-Intersection-Simulator/Assets/Scripts/TrafficManager.cs
+++ SDM-Intersection-Simulator/Assets/Scripts/TrafficManager.cs
@@ -68,25 +68,40 @@ public class TrafficLaneData
 
 public class TrafficManager : Singleton<TrafficManager>
 {
+    public List<TrafficLaneData> carLanes = new List<TrafficLaneData>();
 
-    public List<TrafficLaneData> trafficLanes = new List<TrafficLaneData>();
+    private List<TrafficLaneData> cachedTrafficLanes = new List<TrafficLaneData>();
+    public List<TrafficLaneData> TrafficLanes
+    {
+        get
+        {
+            return cachedTrafficLanes;
 
+        }
+    }
 
     private Client client;
 
     public void Start()
     {
+        PopulateTrafficLanes();
+
         client = GetComponent<Client>();
         SetAllTrafficLights(Trafficlight.eTrafficState.RED);
     }
 
+    private void PopulateTrafficLanes()
+    {
+        cachedTrafficLanes.AddRange(carLanes);
+    }
+
     private void SetAllTrafficLights(Trafficlight.eTrafficState newState)
     {
-        for (int i = 0; i < trafficLanes.Count; i++)
+        for (int i = 0; i < TrafficLanes.Count; i++)
         {
-            for (int j = 0; j < trafficLanes[i].trafficLights.Count; j++)
+            for (int j = 0; j < TrafficLanes[i].trafficLights.Count; j++)
             {
-                Trafficlight trafficLight = trafficLanes[i].trafficLights[j];
+                Trafficlight trafficLight = TrafficLanes[i].trafficLights[j];
                 if (trafficLight == null) continue;
 
                 SetTrafficLightState(trafficLight.Id, newState);
@@ -100,9 +115,9 @@ public class TrafficManager : Singleton<TrafficManager>
     /// </summary>
     public void OnValidate()
     {
-        for (int i = 0; i < trafficLanes.Count; i++)
+        for (int i = 0; i < TrafficLanes.Count; i++)
         {
-            TrafficLaneData trafficLane = trafficLanes[i];
+            TrafficLaneData trafficLane = TrafficLanes[i];
             if (trafficLane == null) continue;
 
             if (trafficLane.trafficLights.Count < 1) return;
@@ -178,9 +193,9 @@ public class TrafficManager : Singleton<TrafficManager>
 
     public void SetTrafficLightState(int id, Trafficlight.eTrafficState newTrafficLightState)
     {
-        for (int i = 0; i < trafficLanes.Count; i++)
+        for (int i = 0; i < TrafficLanes.Count; i++)
         {
-            TrafficLaneData laneData = trafficLanes[i];
+            TrafficLaneData laneData = TrafficLanes[i];
 
             if (laneData == null) continue;
 
@@ -220,9 +235,9 @@ public class TrafficManager : Singleton<TrafficManager>
 
     public TrafficLaneData FindLaneDataById(int id)
     {
-        for (int i = 0; i < trafficLanes.Count; i++)
+        for (int i = 0; i < TrafficLanes.Count; i++)
         {
-            TrafficLaneData trafficLane = trafficLanes[i];
+            TrafficLaneData trafficLane = TrafficLanes[i];
 
             if (trafficLane == null) continue;
 
